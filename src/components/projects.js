8c0fc3a15aff1e88140d2029d85d818a9b504e53
tanthehack <31630356+tanthehack@users.noreply.github.com>
2023-08-17 14:35:34 +0100

    projects section responsiveness

--- src/components/projects.js
+++ src/components/projects.js
@@ -7,12 +7,12 @@ import { Button } from './globals/button'
 export const Projects = () => {
 
     return (
-        <section className="flex flex-col w-[100vw] items-center space-y-[140px] lg:px-12 px-5">
-            <div className='flex items-center justify-between w-full'>
+        <section className="flex flex-col w-[100vw] items-center lg:space-y-[140px] space-y-9 pt-[100px]">
+            <div className='flex flex-col lg:flex-row  lg:items-center justify-between w-full lg:px-12 px-5'>
                 <h2>Our Works</h2>
                 <div className="flex items-center gap-4">
                     View All Projects
-                    <Button icon={<Icon.HiArrowRight />} iconOnly variant="primary-outline" />
+                    <Button icon={<Icon.HiArrowRight className='lg:text-base text-[8px]' />} iconOnly variant="primary-outline" />
                 </div>
             </div>
 
@@ -28,14 +28,14 @@ export const Projects = () => {
                         className='w-full lg:h-[500px] h-[200px]'
                     >
                     </motion.div>
-                    <div className='flex items-center justify-between w-full'>
-                        <div className='flex items-center gap-4'>
+                    <div className='flex items-center justify-between w-full lg:px-12 px-5'>
+                        <div className='flex flex-col lg:flex-row lg:items-center items-start lg:gap-4'>
                             <h3 className='bg-black'>{project.title}</h3>
-                            <p>{project.description}</p>
+                            <p className='lg:text-base text-xs'>{project.description}</p>
                         </div>
 
                         {project.tags.map((tag, index) => (
-                            <span key={index} className='border-[1px] border-white rounded-xl py-1 px-7 uppercase'>{tag}</span>
+                            <span key={index} className='border-[1px] border-white rounded-xl py-1 px-7 uppercase text-xs lg:text-base'>{tag}</span>
                         ))}
                     </div>
                 </div>
