e98e06ec8482b24faa1c2cb630216bc1b069c8ea
tanthehack <31630356+tanthehack@users.noreply.github.com>
2023-08-20 22:40:09 +0100

    fix: services responsiveness

--- src/components/services.js
+++ src/components/services.js
@@ -3,12 +3,11 @@ import Link from "next/link"
 
 export const Services = () => {
     return (
-        <section className="grid grid-cols-2 w-[100vw] h-[100vh] lg:px-12 px-5 py-8 pt-[100px] z-10">
-            <h1 className="lg:leading-[80px] row-start-1">Our<br />Services.</h1>
-
+        <section className="grid lg:grid-cols-2 grid-cols-1 w-[100vw] lg:h-[100vh] h-fit lg:px-12 px-5 py-[100px] z-10 content-center relative">
+            <h1 className="lg:leading-[80px] self-center lg:p-8">Our<br />Services.</h1>
             <div className="flex flex-col h-1/5">
                 {ServicesData.map((item) =>
-                    (<Link href="/work" className="text-white text-3xl p-8 border-b-[0.2px] border-b-white hover:after:content-['↗'] " key={item.id}>(0{item.id}) {item.title}</Link>)
+                    (<Link href="/work" className="text-white lg:text-3xl p-8 lg:mr-8 border-b-[0.2px] border-b-white hover:after:content-['↗'] " key={item.id}>(0{item.id}) {item.title}</Link>)
                 )}
             </div>
         </section>
