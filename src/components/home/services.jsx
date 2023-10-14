8d7e65daf209105beb118b358680d38187699dcc
tanthehack <31630356+tanthehack@users.noreply.github.com>
2023-10-14 19:31:38 +0100

    add: services card anim

--- src/components/home/services.jsx
+++ src/components/home/services.jsx
@@ -1,6 +1,7 @@
 import { useLayoutEffect, useRef } from "react";
 import { ServicesData } from "../../data/servicesdata"
 import gsap from "gsap";
+import { motion } from 'framer-motion'
 import { ScrollTrigger } from "gsap/all";
 import * as Icon from 'react-icons/hi'
 
@@ -21,12 +22,14 @@ export const Services = () => {
     });
 
     return (
-        <section className="flex lg:flex-row flex-col gap-8 w-[100vw] lg:h-screen h-fit lg:px-12 px-5 items-center overflow-x-hidden relative" ref={component}>
-            <h1 className="lg:leading-[80px] leading-none lg:p-8 header top-[100px]">Our<br />Services.</h1>
-            <div className="flex flex-col gap-8">
-                <div className="flex flex-row gap-10 w-[1500px] relative" ref={slider}>
+        <section className="flex lg:flex-row flex-col gap-8 w-[100vw] lg:h-screen h-fit lg:px-12 px-5 items-center overflow-hidden bg-black relative" ref={component}>
+            <div className="bg-black h-full w-full flex items-center justify-center">
+                <h1 className="lg:leading-[80px] leading-none lg:p-8 header top-[100px]">Our<br />Services.</h1>
+            </div>
+            <div className="flex flex-col items-center justify-center gap-8">
+                <div className="flex flex-row gap-10 w-[1500px]" ref={slider}>
                     {ServicesData.map((item) => (
-                        <ServiceCard key={item.id} id={item.id} title={item.title} />)
+                        <ServiceCard key={item.id} id={item.id} title={item.title} desc={item.description} />)
                     )}
                 </div>
 
@@ -43,15 +46,73 @@ export const Services = () => {
     )
 }
 
-export const ServiceCard = ({ id, title }) => {
+export const ServiceCard = ({ id, title, desc }) => {
+    const parentVariants = {
+        variantA: {
+            scale: 1
+        },
+        variantB: {
+            scale: 1.003
+        },
+        transition: {
+            when: "beforeChildren",
+        }
+    }
+
+    const textVariants = {
+        variantA: {
+            x: "-300%",
+            opacity: 0,
+            display: "none"
+        },
+        variantB: {
+            x: 0,
+            opacity: 1,
+            display: "block"
+        },
+        transition: {
+            duration: 0.5,
+            delay: 0.5
+        }
+    }
+
+    const arrowVariants = {
+        variantA: {
+            scale: 0,
+            opacity: 0,
+        },
+        variantB: {
+            scale: 1,
+            opacity: 1,
+        }
+    }
+
     return (
-        <div className="bg-green-500 w-[300px] h-[300px] text-black p-10 rounded-xl hover:h-[400px] transiton ease-in-out duration-300 relative">
+        <motion.div
+            className="bg-green-500 group w-[300px] h-[300px] hover:h-[450px] text-black p-10 rounded-xl transiton ease-in-out duration-300 relative"
+            variants={parentVariants}
+            initial="variantA"
+            whileHover="variantB"
+        >
             <h1 className="text-[70px]">{id}</h1>
             <h3>{title}</h3>
 
-            {/* <span className="bg-green-500 h-[70px] w-[70px] absolute rounded-full flex items-center justify-center border-[4px] border-black -top-10 -right-8">
-                <Icon.HiOutlineArrowsExpand />
-            </span> */}
-        </div>
+            <motion.span className="mt-6 block"
+                variants={textVariants}
+                transition={{
+
+                }}
+            >
+                <p className="text-sm">{desc}</p>
+            </motion.span>
+
+            <motion.span
+                className="bg-white h-[70px] w-[70px] absolute rounded-full flex items-center justify-center border-[4px] border-black -top-10 -right-8"
+                variants={arrowVariants}
+            >
+                {/* <Icon.HiOutlineArrowsExpand /> */}
+                <p>↗</p>
+            </motion.span>
+        </motion.div>
     )
 }
--- src/data/servicesdata.jsx
+++ src/data/servicesdata.jsx
@@ -3,21 +3,26 @@ export const ServicesData = [
     {
         id: 1,
         title: "Graphic Design",
+        description: "Our graphic design service is where pixels meet purpose. We craft visuals that do the talking for you, turning ideas into polished, professional designs."
     },
     {
         id: 2,
         title: "Motion Design",
+        description: "Lights, camera, animation! With motion design, we infuse life into your brand. Our motion graphics add that touch of sophistication to make your content stand out."
     },
     {
         id: 3,
         title: "Branding",
+        description: "We sculpt your brand's unique identity. From logos to fonts and color schemes, we craft a memorable presence that speaks to your audience, fostering brand loyalty and leaving a lasting impression. Your brand, our expertise."
     },
     {
         id: 4,
         title: "Strategy*",
+        description: "Think of us as your navigators in the design universe. We craft strategies that align with your goals, guiding your brand to reach new heights with a sprinkle of creativity and a dash of professionalism."
     },
     {
         id: 5,
-        title: "Event Theming"
+        title: "Event Theming",
+        description: "We transform events into unforgettable experiences. Our creative touch sets the stage, crafting themes and decor that elevate your gatherings and leave a lasting impression."
     }
 ]
