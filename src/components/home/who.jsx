01360c0f31cc55ec389ad7352045711d388069a6
tanthehack <31630356+tanthehack@users.noreply.github.com>
2023-10-14 02:23:31 +0100

    fix: who layout and add: anim

--- src/components/home/who.jsx
+++ src/components/home/who.jsx
@@ -1,20 +1,45 @@
-import { useRef } from "react";
+import { useLayoutEffect, useRef } from "react";
 import { Links } from "../global/links";
-import { useScroll, useTransform, motion } from "framer-motion";
 import * as Icon from "react-icons/hi";
+import meet from '../../assets/meet.png'
+import gsap from "gsap";
 
 export const Who = () => {
 
+    const main = useRef(null);
+
+    useLayoutEffect(() => {
+        const ctx = gsap.context((self) => {
+            gsap.fromTo(main.current,
+                {
+                    backgroundColor: "#000",
+                    color: "#fff"
+                },
+
+                {
+                    backgroundColor: "#97FF80",
+                    color: "#000",
+                    duration: 1,
+                    ease: "ease.out",
+                    scrollTrigger: {
+                        trigger: main.current,
+                        start: "30% center",
+                        end: "50% center",
+                        scrub: 1,
+                    }
+                })
+        }, main)
+
+        return () => ctx.revert();
+    }, [])
+
     return (
         <section
-            className="h-[100vh] text-black bg-green-500 lg:rounded-[100px] rounded-[30px] lg:mx-10 mx-2"
+            className="h-[150vh] text-black bg-green-500 flex flex-col items-center justify-center gap-8"
+            ref={main}
         >
-            <div
-                className="flex flex-col items-center justify-center space-y-8 relative h-full"
-            >
-                <h2 className="lg:w-3/5 w-2/3 text-center">A diverse design agency passionate about creating digital experiences.</h2>
-                <Links icon={<Icon.HiArrowRight />} variant="primary-black" children={'who we are.'} />
-            </div>
+            <h1 className="leading-none text-center">A diverse design agency passionate about creating digital experiences.</h1>
+            <Links icon={<Icon.HiArrowRight />} variant="primary-black" children={'who we are.'} />
         </section>
     )
 }
