b4ef36af508460ebd7a30964040b8adc41c73655
tanthehack <31630356+tanthehack@users.noreply.github.com>
2023-12-29 13:15:34 +0100

    add: all works styling and content

--- src/routes/works.jsx
+++ src/routes/works.jsx
@@ -1,35 +1,51 @@
-import { Outlet, useParams } from "react-router-dom"
+import { Link, Outlet, useParams } from "react-router-dom"
 import { WorkData as works } from "../data/worksdata"
 
 export const Works = () => {
     const { id } = useParams()
 
-    let allWorks = <div className="p-10">
-        <h1 className="py-10">Our Works</h1>
-        <ul className="grid grid-cols-1 lg:grid-cols-2 gap-4">
+    let allWorks = <div className="p-10 lg:p-20">
+        <ul className="grid grid-cols-1 lg:grid-cols-2 gap-2 items-center">
+            <h1 className="text-6xl md:text-7xl lg:text-9xl leading-none">Our <br /> Works</h1>
             {
                 works.map((work, index) => (
                     <li key={index} className="space-y-3">
-                        <div className="flex flex-col gap-4 rounded-lg group overflow-hidden hover:cursor-pointer"
-                            style={{
-                                backgroundImage: `url(${work.header})`,
-                                backgroundSize: 'cover',
-                                backgroundPosition: 'center',
-                                backgroundRepeat: 'no-repeat',
-                                height: '200px'
-                            }}
-                        >
-                            <div className="group-hover:opacity-100 absolute top-0 left-0 opacity-0 transition duration-500 ease-in-out text-2xl flex items-center justify-center bg-black backdrop-blur-sm bg-opacity-40 h-full w-full font-display">
-                                <p className="text-white text-2xl font-display">See Project</p>
-                            </div>
-                        </div>
-                        <h3 className="text-white">{work.title}</h3>
+                        {
+                            work.display.includes('.mp4') ?
+                                <div className="flex flex-col gap-4 group overflow-hidden hover:cursor-pointer rounded-xl relative h-[200px] lg:h-[500px]">
+                                    <video
+                                        src={work.display}
+                                        alt={work.title}
+                                        autoPlay
+                                        loop
+                                        muted
+                                        className="w-full h-full object-cover"
+                                    />
+                                    <Link to={'/works/' + work.workTitle} className="group-hover:opacity-100 absolute top-0 left-0 opacity-0 transition duration-500 ease-in-out text-2xl flex items-center justify-center bg-black backdrop-blur-sm bg-opacity-40 h-full w-full font-display">
+                                        <p className="text-white text-2xl font-display">{work.title}</p>
+                                    </Link>
+                                </div>
+                                :
+                                <div className="flex flex-col gap-4 group overflow-hidden hover:cursor-pointer rounded-xl relative h-[200px] lg:h-[500px]"
+                                    style={{
+                                        backgroundImage: `url(${work.display})`,
+                                        backgroundSize: 'cover',
+                                        backgroundPosition: 'center',
+                                        backgroundRepeat: 'no-repeat',
+                                    }}
+                                >
+                                    <Link to={'/works/' + work.workTitle} className="group-hover:opacity-100 absolute top-0 left-0 opacity-0 transition duration-500 ease-in-out text-2xl flex items-center justify-center bg-black backdrop-blur-sm bg-opacity-40 h-full w-full font-display">
+                                        <p className="text-white text-2xl font-display">{work.title}</p>
+                                    </Link>
+                                </div>
+                        }
+                        {/* <h3 className="text-white">{work.title}</h3>
                         <div className="flex items-center gap-4">
                             {
                                 work.categories.map((category, index) => (
-                                    <span className="font-thin border-[1px] border-white rounded-lg py-1 px-7 uppercase text-[8px] lg:text-xs" key={index}>{category}</span>))
+                                    <span className="font-thin border-[1px] border-grey text-grey rounded-lg py-1 px-7 uppercase text-[8px] lg:text-xs" key={index}>{category}</span>))
                             }
-                        </div>
+                        </div> */}
                     </li>
                 ))
             }
