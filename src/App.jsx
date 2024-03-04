9df370b4425ceb3e96f7e0bb03b472cb31323c6f
tanthehack <31630356+tanthehack@users.noreply.github.com>
2024-03-04 11:40:34 +0100

    fix: removed lenis scoll, sped up site

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,8 @@
 import { Outlet, ScrollRestoration } from "react-router-dom"
 import { Layout } from "./components/layouts/layout"
-import gsap from "gsap"
-import { ScrollTrigger } from "gsap/all"
-import Lenis from "@studio-freight/lenis"
 import ScrollToTop from "./components/global/scrollToTop"
 
 function App() {
-
-  // gsap.registerPlugin(ScrollTrigger)
-
-  // const lenis = new Lenis()
-  // lenis.on('scroll', ScrollTrigger.update)
-
-  // gsap.ticker.add((time) => {
-  //   lenis.raf(time * 1000)
-  // })
-
-  // gsap.ticker.lagSmoothing(0)
 
   return (
     <div className="relative">
