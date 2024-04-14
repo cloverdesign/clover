a44318587f00a6104567e0ccc7126f26d3d8fb11
tanthehack <31630356+tanthehack@users.noreply.github.com>
2024-04-14 11:52:56 +0100

    fix: contact page responsiveness

--- src/routes/contact.jsx
+++ src/routes/contact.jsx
@@ -1,15 +1,15 @@
 export const Contact = () => {
   return (
-    <section className="p-10 m-20 mt-[150px] h-[500px] flex flex-col items-center justify-center bg-green-500 rounded-xl text-black">
+    <section className="p-10 m-5 lg:m-20 mt-[150px] h-[300px] lg:h-[500px] flex flex-col items-center justify-center bg-green-500 rounded-xl text-black">
       <div className="space-y-8">
-        <h1 className="leading-none text-8xl">
+        <h1 className="leading-none text-4xl md:text-6xl lg:text-8xl">
           Let's work <br /> together
         </h1>
-        <span className="flex items-center gap-5">
-          <p className="text-xl font-thin">Email us at</p>
+        <span className="flex flex-col md:flex-row md:items-center gap-5">
+          <p className="lg:text-xl font-thin">Email us at</p>
           <a
             href="mailto:cloverdesignhq@gmail.com"
-            className="bg-black text-green-500 py-2 px-6 rounded-lg text-xl lg:text-2xl"
+            className="bg-black text-green-500 py-2 px-6 rounded-lg text-base lg:text-2xl"
           >
             cloverdesignhq@gmail.com
           </a>
