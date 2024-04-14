56114387cc02c99b384ff2d632162539401dedc0
tanthehack <31630356+tanthehack@users.noreply.github.com>
2024-04-14 10:52:41 +0100

    fix: links on home

--- src/components/home/who.jsx
+++ src/components/home/who.jsx
@@ -1,45 +1,54 @@
 import { useLayoutEffect, useRef } from "react";
 import { Links } from "../global/links";
 import * as Icon from "react-icons/hi";
-import meet from '../../assets/meet.png'
+import meet from "../../assets/meet.png";
 import gsap from "gsap";
 
 export const Who = () => {
+  const main = useRef(null);
 
-    const main = useRef(null);
+  useLayoutEffect(() => {
+    const ctx = gsap.context((self) => {
+      gsap.fromTo(
+        main.current,
+        {
+          backgroundColor: "#000",
+          color: "#fff",
+        },
 
-    useLayoutEffect(() => {
-        const ctx = gsap.context((self) => {
-            gsap.fromTo(main.current,
-                {
-                    backgroundColor: "#000",
-                    color: "#fff"
-                },
+        {
+          backgroundColor: "#97FF80",
+          color: "#000",
+          duration: 1,
+          ease: "ease.out",
+          scrollTrigger: {
+            trigger: main.current,
+            start: "10% center",
+            end: "50% center",
+            scrub: 1,
+          },
+        },
+      );
+    }, main);
 
-                {
-                    backgroundColor: "#97FF80",
-                    color: "#000",
-                    duration: 1,
-                    ease: "ease.out",
-                    scrollTrigger: {
-                        trigger: main.current,
-                        start: "10% center",
-                        end: "50% center",
-                        scrub: 1,
-                    }
-                })
-        }, main)
+    return () => ctx.revert();
+  }, []);
 
-        return () => ctx.revert();
-    }, [])
-
-    return (
-        <section
-            className="h-[100vh] text-black bg-green-500 flex flex-col items-center justify-center gap-8"
-            ref={main}
-        >
-            <h1 className="leading-none text-center">A diverse design agency passionate about creating digital experiences.</h1>
-            <Links icon={<Icon.HiArrowRight />} variant="primary-black" children={'who we are.'} />
-        </section>
-    )
-}
+  return (
+    <section
+      className="h-[100vh] text-black bg-green-500 flex flex-col items-center justify-center gap-8"
+      ref={main}
+    >
+      <h1 className="leading-none text-center">
+        A diverse design agency passionate about creating digital experiences.
+      </h1>
+      <Links
+        icon={<Icon.HiArrowRight />}
+        variant="primary-black"
+        url="/about/#who"
+      >
+        who we are.
+      </Links>
+    </section>
+  );
+};
