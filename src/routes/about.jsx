ebc31b69b8d9054dec2cf742c323afc397868bf9
tanthehack <31630356+tanthehack@users.noreply.github.com>
2024-04-14 17:01:43 +0100

    fix: team layout and added members

--- src/routes/about.jsx
+++ src/routes/about.jsx
@@ -70,15 +70,18 @@ export const About = () => {
         </div>
       </section>
 
-      <section id="the-team" className="flex flex-wrap gap-10 justify-center">
-        <div className="group mb-10 w-[200px] lg:w-[400px] place-content-center">
+      <section
+        id="the-team"
+        className="pt-20 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 lg:gap-10 justify-center place-items-center"
+      >
+        <div className="group mb-10 flex flex-col lg:flex-row justify-center lg:items-center md:col-span-1 lg:col-span-3 gap-4 lg:gap-10 w-[300px] lg:w-full">
           <span className="relative">
             <span className="bg-green-500 group-hover:rotate-[8deg] transition duration-500 ease-in-out text-black p-2 rounded-lg absolute -rotate-[8deg] -top-8 left-0 text-xs lg:text-base">
               design culprits
             </span>
-            <h1 className="lg:w-[50%] w-full leading-none">The Team</h1>
+            <h1 className="w-full leading-none">The Team</h1>
           </span>
-          <p className="font-thin mt-10 text-xs lg:text-base">
+          <p className="font-thin text-xs lg:text-base lg:w-[50%]">
             We are a team of creatives who are excited about unique ideas and
             help brands to create amazing identities.
           </p>
@@ -90,8 +93,12 @@ export const About = () => {
               hoverImg={member.second_pic}
               classNames="mb-5"
             />
-            <h4 className="font-display max-w-[200px]">{member.name}</h4>
-            <p className="font-thin text-sm text-grey">{member.role}</p>
+            <h4 className="font-display lg:max-w-[400px] max-w-[200px]">
+              {member.name}
+            </h4>
+            <p className="font-thin lg:max-w-[400px] max-w-[200px] text-sm text-grey">
+              {member.role}
+            </p>
           </div>
         ))}
       </section>
@@ -102,24 +109,44 @@ export const About = () => {
 const Picture = ({ img, hoverImg, classNames }) => {
   return (
     <div className="group">
-      <div
-        className={`lg:w-[400px] w-[200px] lg:h-[400px] h-[200px] bg-team group-hover:hidden hover:bg-team-hover bg-cover rounded-xl ${classNames}`}
-        style={{
-          backgroundImage: `url(${img})`,
-          backgroundSize: "cover",
-          backgroundPosition: "center",
-          backgroundRepeat: "no-repeat",
-        }}
-      ></div>
-      <div
-        className={`lg:w-[400px] w-[200px] lg:h-[400px] h-[200px] bg-cover group-hover:flex hidden rounded-xl ${classNames}`}
-        style={{
-          backgroundImage: `url(${hoverImg})`,
-          backgroundSize: "cover",
-          backgroundPosition: "center",
-          backgroundRepeat: "no-repeat",
-        }}
-      ></div>
+      {img !== "" ? (
+        <>
+          <div
+            className={`lg:w-[400px] md:w-[300px] md:h-[300px] w-[200px] lg:h-[400px] h-[200px] bg-team group-hover:hidden hover:bg-team-hover bg-cover rounded-xl ${classNames}`}
+            style={{
+              backgroundImage: `url(${img})`,
+              backgroundSize: "cover",
+              backgroundPosition: "center",
+              backgroundRepeat: "no-repeat",
+            }}
+          ></div>
+          <div
+            className={`lg:w-[400px] md:w-[300px] md:h-[300px] w-[200px] lg:h-[400px] h-[200px] bg-cover group-hover:flex hidden rounded-xl ${classNames}`}
+            style={{
+              backgroundImage: `url(${hoverImg})`,
+              backgroundSize: "cover",
+              backgroundPosition: "center",
+              backgroundRepeat: "no-repeat",
+            }}
+          ></div>
+        </>
+      ) : (
+        <div
+          className={`lg:w-[400px] md:w-[300px] md:h-[300px] w-[200px] lg:h-[400px] h-[200px] bg-white rounded-xl flex items-center justify-center text-black ${classNames}`}
+        >
+          <svg
+            className={`w-[35px] fill-black transition duration-500 ease-in-out `}
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 904.01 856.44"
+          >
+            <g id="Layer_2" data-name="Layer 2">
+              <g id="Layer_1-2" data-name="Layer 1">
+                <polygon points="785.53 743.9 686.53 662.94 733.85 780.08 624.94 856.44 452 569.75 279.07 856.44 170.15 780.08 217.47 662.94 118.48 743.9 0 660.88 124.29 456.48 268.78 427.92 128.64 400.93 7.5 186.61 124.89 109.88 216.5 184.43 184.91 70.55 292.74 0 452 284.27 611.26 0 719.09 70.55 687.5 184.43 779.11 109.88 896.5 186.61 775.24 400.93 635.22 427.92 779.72 456.48 904 660.88 785.53 743.9" />
+              </g>
+            </g>
+          </svg>
+        </div>
+      )}
     </div>
   );
 };
