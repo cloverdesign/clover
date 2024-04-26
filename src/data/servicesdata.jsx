02a8ce78667b43e2d0b42cfd77198e11d7fec86e
tanthehack <31630356+tanthehack@users.noreply.github.com>
2024-04-26 13:00:01 +0100

    fix: removed asterisk in strategy

--- src/data/servicesdata.jsx
+++ src/data/servicesdata.jsx
@@ -1,28 +1,32 @@
-
 export const ServicesData = [
-    {
-        id: 1,
-        title: "Graphic Design",
-        description: "Where pixels meet purpose. We craft visuals that do the talking for you, turning ideas into polished, professional designs."
-    },
-    {
-        id: 2,
-        title: "Motion Design",
-        description: "Lights, camera, animation! With motion design, we infuse life into your brand."
-    },
-    {
-        id: 3,
-        title: "Branding",
-        description: "We sculpt your brand's unique identity. Crafting a memorable presence that speaks to your audience, fostering brand loyalty and leaving a lasting impression. Your brand, our expertise."
-    },
-    {
-        id: 4,
-        title: "Strategy*",
-        description: "Think of us as your navigators in the design universe. We craft strategies that align with your goals, guiding your brand to reach new heights with a sprinkle of creativity and a dash of professionalism."
-    },
-    {
-        id: 5,
-        title: "Event Theming",
-        description: "Unforgettable experiences. Our creative touch sets the stage, crafting themes and decor that elevate your gatherings and leave a lasting impression."
-    }
-]
+  {
+    id: 1,
+    title: "Graphic Design",
+    description:
+      "Where pixels meet purpose. We craft visuals that do the talking for you, turning ideas into polished, professional designs.",
+  },
+  {
+    id: 2,
+    title: "Motion Design",
+    description:
+      "Lights, camera, animation! With motion design, we infuse life into your brand.",
+  },
+  {
+    id: 3,
+    title: "Branding",
+    description:
+      "We sculpt your brand's unique identity. Crafting a memorable presence that speaks to your audience, fostering brand loyalty and leaving a lasting impression. Your brand, our expertise.",
+  },
+  {
+    id: 4,
+    title: "Strategy",
+    description:
+      "Think of us as your navigators in the design universe. We craft strategies that align with your goals, guiding your brand to reach new heights with a sprinkle of creativity and a dash of professionalism.",
+  },
+  {
+    id: 5,
+    title: "Event Theming",
+    description:
+      "Unforgettable experiences. Our creative touch sets the stage, crafting themes and decor that elevate your gatherings and leave a lasting impression.",
+  },
+];
